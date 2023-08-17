81c253893b1faccfae3555dedf22af8336ef3497
SIMJIYEON93 <930515s@gmail.com>
2023-08-17 10:40:53 +0900

    Initial commit

---
+++ src/main/java/com/bb3/bodybuddybe/BodyBuddyBeApplication.java
@@ -0,0 +1,13 @@
+package com.bb3.bodybuddybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyBuddyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodyBuddyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bb3/bodybuddybe/BodyBuddyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb3.bodybuddybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyBuddyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
