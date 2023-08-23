9340e5dfa1cddbfd23782e47b9aa08e6d0d0f704
SIMJIYEON93 <930515s@gmail.com>
2023-08-23 13:57:39 +0900

    feat: import 수정

--- src/main/java/com/bb3/bodybuddybe/BodyBuddyBeApplication.java
+++ src/main/java/com/bb3/bodybuddybe/BodyBuddyBeApplication.java
@@ -1,5 +1,6 @@
 package com.bb3.bodybuddybe;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BodyBuddyBeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BodyBuddyBeApplication.class, args);
     }
 
