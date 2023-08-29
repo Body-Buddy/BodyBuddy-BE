de89ecfce55dec925288550b27f088d5b131a780
SIMJIYEON93 <930515s@gmail.com>
2023-08-29 13:03:18 +0900

    feat: 소셜 로그인 config 설정

--- src/main/java/com/bb3/bodybuddybe/common/config/WebSecurityConfig.java
+++ src/main/java/com/bb3/bodybuddybe/common/config/WebSecurityConfig.java
@@ -4,8 +4,6 @@ import com.bb3.bodybuddybe.common.jwt.JwtUtil;
 import com.bb3.bodybuddybe.common.security.JwtAuthenticationFilter;
 import com.bb3.bodybuddybe.common.security.JwtAuthorizationFilter;
 import com.bb3.bodybuddybe.common.security.UserDetailsServiceImpl;
-import com.bb3.bodybuddybe.common.security.UserVerificationFilter;
-import com.bb3.bodybuddybe.user.oauth2.handler.JwtService;
 import com.bb3.bodybuddybe.user.oauth2.handler.OAuth2LoginFailureHandler;
 import com.bb3.bodybuddybe.user.oauth2.handler.OAuth2LoginSuccessHandler;
 import com.bb3.bodybuddybe.user.oauth2.service.CustomOAuth2UserService;
@@ -35,7 +33,6 @@ public class WebSecurityConfig {
     private final ObjectMapper objectMapper;
     private final AuthenticationConfiguration authenticationConfiguration;
     private final UserRepository userRepository;
-    private final JwtService jwtService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -56,20 +53,29 @@ public class WebSecurityConfig {
         return new JwtAuthorizationFilter(jwtUtil, userDetailsService, objectMapper);
     }
 
+    @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
         JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRepository);
         filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
         return filter;
     }
 
-    public UserVerificationFilter userVerificationFilter() {
-        return new UserVerificationFilter(objectMapper);
-    }
+
 
     @Bean //security filter
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF, CORS 설정
         http.csrf(csrf -> csrf.disable());
+        http.oauth2Login()
+                .successHandler(OAuth2LoginSuccessHandler())
+                .failureHandler(OAuth2LoginFailureHandler())
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
         http.cors(corf -> corf.disable());
 
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
@@ -85,6 +91,6 @@ public class WebSecurityConfig {
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(userVerificationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
 }}
