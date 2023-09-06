9acb0d7454d0beea326855b994646c207d0b7c1a
lss6181 <dexter5ya@naver.com>
2023-09-06 15:08:43 +0900

    fix: ChatRepository.findAllByGym_IdAndOwnerUserAndChatType_Direct(Long gymId, User user); 오류 -> findAllByGym_IdAndOwnerUserAndChatTypeContains(Long gymId, User user, ChatType chatType); 변경

--- src/main/java/com/bb3/bodybuddybe/chat/repository/ChatRepository.java
+++ src/main/java/com/bb3/bodybuddybe/chat/repository/ChatRepository.java
@@ -1,6 +1,7 @@
 package com.bb3.bodybuddybe.chat.repository;
 
 import com.bb3.bodybuddybe.chat.entity.Chat;
+import com.bb3.bodybuddybe.chat.entity.ChatType;
 import com.bb3.bodybuddybe.user.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findAllByGym_Id(Long gymId);
 
-    List<Chat> findAllByGym_IdAndOwnerUserAndChatType_Direct(Long gymId, User user);
+    List<Chat> findAllByGym_IdAndOwnerUserAndChatTypeContains(Long gymId, User user, ChatType chatType);
+
 }
