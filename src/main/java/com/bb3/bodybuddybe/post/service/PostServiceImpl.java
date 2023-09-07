d223b766f0d49cd9f494a88ba90b90bddf570871
endrmseha <133080680+endrmseha@users.noreply.github.com>
2023-09-07 14:22:27 +0900

    feat: 게시글 이미지 등록 (#47)
    * feat: 게시글 이미지 등록 1차
    
    * feat: 게시글 이미지 등록 2차
    
    * feat: 게시글 이미지 등록 3차
    
    * feat: 게시글 이미지 등록 최종완료

--- src/main/java/com/bb3/bodybuddybe/post/service/PostServiceImpl.java
+++ src/main/java/com/bb3/bodybuddybe/post/service/PostServiceImpl.java
@@ -2,8 +2,11 @@ package com.bb3.bodybuddybe.post.service;
 
 import com.bb3.bodybuddybe.common.exception.CustomException;
 import com.bb3.bodybuddybe.common.exception.ErrorCode;
+import com.bb3.bodybuddybe.common.image.ImageUploader;
 import com.bb3.bodybuddybe.gym.entity.Gym;
 import com.bb3.bodybuddybe.gym.repository.GymRepository;
+import com.bb3.bodybuddybe.image.entity.Image;
+import com.bb3.bodybuddybe.image.repository.ImageRepository;
 import com.bb3.bodybuddybe.post.dto.CategoryResponseDto;
 import com.bb3.bodybuddybe.post.dto.PostCreateRequestDto;
 import com.bb3.bodybuddybe.post.dto.PostResponseDto;
@@ -17,6 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,10 +31,12 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final GymRepository gymRepository;
+    private final ImageUploader imageUploader;
+    private final ImageRepository imageRepository;
 
     @Override
     @Transactional
-    public void createPost(PostCreateRequestDto requestDto, User user) {
+    public void createPost(PostCreateRequestDto requestDto, User user, List<MultipartFile> files) {
         Gym gym = findGym(requestDto.getGymId());
 
         Post post = Post.builder()
@@ -42,6 +48,15 @@ public class PostServiceImpl implements PostService {
                 .build();
 
         postRepository.save(post);
+        if (files != null) {
+            for (MultipartFile file : files) {
+                String fileUrl = imageUploader.upload(file);
+                if (imageRepository.existsByImageUrlAndId(fileUrl, post.getId())) {
+                    throw new CustomException(ErrorCode.FILE_ALREADY_EXISTS);
+                }
+                imageRepository.save(Image.builder().imageUrl(fileUrl).post(post).build());
+            }
+        }
     }
 
     @Override
