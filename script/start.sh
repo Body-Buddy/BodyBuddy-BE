1224f948f815e924b401e48ed44dcef897a7bab8
SIMJIYEON93 <111446820+SIMJIYEON93@users.noreply.github.com>
2023-09-16 16:25:32 +0900

    Feature/43 chat reject chat (#85)
    * feat: 인가 username->email로 변경
    
    * feat: 인가 username->email로 변경
    
    * feat: 인가 username->email로 변경
    
    * feat: 설정파일 삭제
    
    * feat: test 설정파일 삭제
    
    * feat: 파일 디렉토리변경
    
    * feat: 파일 디렉토리변경
    
    * feat: 코드디플로이 적용
    
    * feat: yml수정
    
    * feat: test
    
    * feat: jar파일수정
    
    * feat: test
    
    * feat: test
    
    * feat: test
    
    * feat: test
    
    * feat: test

--- script/start.sh
+++ script/start.sh
@@ -3,7 +3,6 @@
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/BodyBuddy-BE-0.0.1-SNAPSHOT.jar"
 
-
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
@@ -16,7 +15,7 @@ cp $PROJECT_ROOT/build/libs/BodyBuddy-BE-0.0.1-SNAPSHOT.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE & > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
